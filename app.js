673212053aec643828a99854bda178b6a17e851f
rituisboy <riturajthakur100@gmail.com>
2024-03-15 17:47:23 +0530

    'dummy'

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
 app.use(cors());
-app.use(express.json())
-require('dotenv').config()
+app.use(express.json());
+require("dotenv").config();
 const PORT = 4002;
 
-const music = require('./routes/routes')
-app.use('/music',music)
+const { Music } = require("./models/music");
+const music = require("./routes/routes");
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{console.log('successfully connected to the cloud database')
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`)
-})})
-.catch((error)=>{console.log(error);})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("successfully connected to the cloud database");
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-
-app.get('/',(req,res)=>{
-    res.status(200).send('Welcome to RAS Music, a mern stack project')
-})
+app.get("/", async (req, res) => {
+  res.status(200).send("Welcome to RAS Music, a mern stack project");
+});
 
 // test
